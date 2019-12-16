285002c13147eb0a090641f11f2bf5bda222b89c
Matthew Pierce <mapierce@tcd.ie>
2019-12-16 15:11:34 +0000

    Added tests for function and dot separator

--- LineBreaker/LineBreakerTests/LineBreakerTests.swift
+++ LineBreaker/LineBreakerTests/LineBreakerTests.swift
@@ -13,10 +13,10 @@ final class LineBreakerTests: XCTestCase {
     
     private struct Constants {
 
-        static let funcNameOne = "func fetchCities(withAdType adType: DaftAdTypeType, someTuple t: (id: Int, name: String), adCreation: Bool, completion: @escaping (QueryAreaGroup?, _ error: APIError?) -> ()) {"
+        static let funcNameOne = "func fetchCities(withAdType adType: DaftAdTypeType,someTuple t: (id: Int, name: String), adCreation: Bool, completion: @escaping (QueryAreaGroup?, _ error: APIError?) -> ()) {"
         static let funcNameTwo = "internal class func persistCustomLocations(_ customLocations: Array<CustomLocation>, url: URL? = defaultURL, completion: @escaping (_ completed: Bool) -> Void) {"
-        static let funcNameThree = "func load<T>(_ resource: Resource<T>, completion: @escaping (Result<T, APIError>) -> Void) {"
-        static let funcNameFour = "func presentInNewWindow(_ viewController: UIViewController, animated: Bool, completion: (() -> Void)?) {"
+        static let funcNameThree = "func load<T>(_ resource: Resource<T>,completion: @escaping (Result<T, APIError>) -> Void) {"
+        static let funcNameFour = "func presentInNewWindow(_ viewController: UIViewController,animated: Bool,completion: (() -> Void)?) {"
         static let funcNameFive = "init(withBaseURL baseURL: String, withWebService webService: WebService, withUserCredential userCredential: UserCredentialProtocol) {"
         static let chainedFunctionOne = #"    Image("turtlerock").clipShape(Circle()).overlay(Circle().stroke(Color.white, lineWidth: 4)).shadow(radius: 10)"#
         static let chainedFunctionTwo = "let dotIndexes = line.enumerated().compactMap { $0.element == Constants.dot ? $0.offset : nil }.filter { line[0..<$0].filter { $0 == Constants.openBracket }.count == line[0..<$0].filter { $0 == Constants.closedBracket }.count }.map { $0 - 1}"
@@ -80,6 +80,75 @@ final class LineBreakerTests: XCTestCase {
         XCTAssertTrue(lineBreakerFactory.getLineBreaker(from: Constants.guardTwo) is StandardGuardBreaker)
         XCTAssertTrue(lineBreakerFactory.getLineBreaker(from: Constants.ifOne) is StandardIfBreaker)
         XCTAssertTrue(lineBreakerFactory.getLineBreaker(from: Constants.ifTwo) is StandardIfBreaker)
+    }
+    
+    func testFunctionBreaker() {
+        let funcOneExpectedLine = """
+            func fetchCities(withAdType adType: DaftAdTypeType,
+                             someTuple t: (id: Int, name: String),
+                             adCreation: Bool,
+                             completion: @escaping (QueryAreaGroup?, _ error: APIError?) -> ()) {
+            """
+        let funcTwoExpectedLine = """
+            internal class func persistCustomLocations(_ customLocations: Array<CustomLocation>,
+                                                       url: URL? = defaultURL,
+                                                       completion: @escaping (_ completed: Bool) -> Void) {
+            """
+        let funcThreeExpectedLine = """
+            func load<T>(_ resource: Resource<T>,
+                         completion: @escaping (Result<T, APIError>) -> Void) {
+            """
+        let funcFourExpectedLine = """
+            func presentInNewWindow(_ viewController: UIViewController,
+                                    animated: Bool,
+                                    completion: (() -> Void)?) {
+            """
+        let funcFiveExpectedLine = """
+            init(withBaseURL baseURL: String,
+                 withWebService webService: WebService,
+                 withUserCredential userCredential: UserCredentialProtocol) {
+            """
+        let breaker = FunctionDefinitionBreaker()
+        let brokenLineOne = breaker.breakLine(Constants.funcNameOne)
+        let brokenLineTwo = breaker.breakLine(Constants.funcNameTwo)
+        let brokenLineThree = breaker.breakLine(Constants.funcNameThree)
+        let brokenLineFour = breaker.breakLine(Constants.funcNameFour)
+        let brokenLineFive = breaker.breakLine(Constants.funcNameFive)
+        XCTAssertEqual(brokenLineOne, funcOneExpectedLine)
+        XCTAssertEqual(brokenLineTwo, funcTwoExpectedLine)
+        XCTAssertEqual(brokenLineThree, funcThreeExpectedLine)
+        XCTAssertEqual(brokenLineFour, funcFourExpectedLine)
+        XCTAssertEqual(brokenLineFive, funcFiveExpectedLine)
+    }
+    
+    func testDotBreaker() {
+        let brokenOneExpected = """
+                Image("turtlerock")
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                    .shadow(radius: 10)
+            """
+        let brokenTwoExpected = """
+            let dotIndexes = line
+                .enumerated()
+                .compactMap { $0.element == Constants.dot ? $0.offset : nil }
+                .filter { line[0..<$0].filter { $0 == Constants.openBracket }.count == line[0..<$0].filter { $0 == Constants.closedBracket }.count }
+                .map { $0 - 1}
+            """
+        let brokenThreeExpected = """
+            let invokedSelectors: [Selector] = self
+                ._methodInvokedForSelector
+                .values
+                .filter { $0.hasObservers }
+                .map { $0.selector }
+            """
+        let breaker = DotSeparatorBreaker()
+        let brokenLineOne = breaker.breakLine(Constants.chainedFunctionOne)!
+        let brokenLineTwo = breaker.breakLine(Constants.chainedFunctionTwo)!
+        let brokenLineThree = breaker.breakLine(Constants.chainedFunctionThree)!
+        XCTAssertEqual(brokenLineOne, brokenOneExpected)
+        XCTAssertEqual(brokenLineTwo, brokenTwoExpected)
+        XCTAssertEqual(brokenLineThree, brokenThreeExpected)
     }
 
 }
