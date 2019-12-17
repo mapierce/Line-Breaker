7e9d67ecd5b33a4fe2c77be428e101a7d2456f1b
Matthew Pierce <mapierce@tcd.ie>
2019-12-17 18:27:16 +0000

    Updated collection breaker to work with returns too

--- LineBreaker/LineBreakerSourceEditorCommand/Line Breaker/Line Breaker Factory/Line Breakers/Implementations/CollectionBreaker.swift
+++ LineBreaker/LineBreakerSourceEditorCommand/Line Breaker/Line Breaker Factory/Line Breakers/Implementations/CollectionBreaker.swift
@@ -13,15 +13,16 @@ struct CollectionBreaker: LineBreakerProtocol {
     // MARK: - LineBreaker methods
 
     func breakLine(_ line: String) -> String? {
-        let equalsIndex = line.firstIntIndex(of: StringConstants.equals)
-        let arrayBody = line[equalsIndex..<line.count]
+        guard let beginningIndex = getStartIndex(from: line) else { return nil }
+        let arrayBody = line[beginningIndex..<line.count]
         let commaIndexes = arrayBody.enumerated()
             .compactMap { $0.element == StringConstants.comma ? $0.offset : nil }
             .filter { topLevelCommas(in: arrayBody[0..<$0]) }
-            .map { $0 + equalsIndex }
-        let openSquareIndex = arrayBody.firstIntIndex(of: StringConstants.openSquareBracket) + 1
+            .map { $0 + beginningIndex }
+        guard let openSquareIndex = arrayBody.firstIntIndex(of: StringConstants.openSquareBracket) else { return nil }
         let splitString = line.split(at: commaIndexes)
-        let spaceString = String(repeating: " ", count: openSquareIndex + equalsIndex)
+        guard splitString.count > 1 else { return nil }
+        let spaceString = String(repeating: " ", count: openSquareIndex + 1 + beginningIndex)
         return splitString[0] +
             StringConstants.newLine +
             splitString[1..<splitString.count - 1].map { formatString($0, with: spaceString) }.joined() +
@@ -31,8 +32,19 @@ struct CollectionBreaker: LineBreakerProtocol {
     
     // MARK: - Private functions
     
+    private func getStartIndex(from line: String) -> Int? {
+        if let index = line.firstIntIndex(of: StringConstants.equals) {
+            return index
+        } else if let range = line.range(of: StringConstants.return) {
+            return line.distance(from: line.startIndex, to: range.upperBound)
+        } else {
+            return nil
+        }
+    }
+    
     private func topLevelCommas(in string: String) -> Bool {
         return string.matchCounts(of: StringConstants.openBracket, with: StringConstants.closedBracket) &&
+            string.matchCounts(of: StringConstants.openCurly, with: StringConstants.closedCurly) &&
             string.filter { $0 == StringConstants.quote }.count.isMultiple(of: 2)
     }
     
