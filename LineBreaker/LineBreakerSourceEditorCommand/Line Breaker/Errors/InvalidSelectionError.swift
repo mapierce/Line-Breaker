383f4924cfcdbba4b1e4683ffb253f7867624c3d
Matthew Pierce <mapierce@tcd.ie>
2019-12-18 12:23:37 +0000

    Added the invalid selection error

---
+++ LineBreaker/LineBreakerSourceEditorCommand/Line Breaker/Errors/InvalidSelectionError.swift
@@ -0,0 +1,16 @@
+//
+//  InvalidSelectionError.swift
+//  LineBreakerSourceEditorCommand
+//
+//  Created by Matthew Pierce on 18/12/2019.
+//  Copyright © 2019 Matthew Pierce. All rights reserved.
+//
+
+import Foundation
+
+struct InvalidSelectionError: Error {
+    
+    let code = 0
+    let localizedDescription = "Invalid selection"
+    
+}
